ae9f90c2e41c620b53d9564d13df75661355ffc3
Matt Knight <mattnite@protonmail.com>
2022-07-25 19:03:22 -0700

    no tests

--- build.zig
+++ build.zig
@@ -8,14 +8,4 @@ pub fn build(b: *std.build.Builder) void {
     lpc_patch_elf.setBuildMode(mode);
     lpc_patch_elf.setTarget(target);
     lpc_patch_elf.install();
-
-    const lib = b.addStaticLibrary("lpcboot", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.install();
-
-    const main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
 }
